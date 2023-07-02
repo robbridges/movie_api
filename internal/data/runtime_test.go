55222dd7c7f22c66d60132c8a95784a6a103e7b6
Rob Bridges <Rmb101187@gmail.com>
2023-07-01 20:18:55 -0700

    Table test for badly marshaleld Json

--- internal/data/runtime_test.go
+++ internal/data/runtime_test.go
@@ -20,16 +20,42 @@ func TestRuntime_MarshalJSON(t *testing.T) {
 }
 
 func TestRuntime_UnmarshalJSON(t *testing.T) {
-	jsonData := []byte(`"120 mins"`)
+	t.Run("Valid json happy path", func(t *testing.T) {
+		jsonData := []byte(`"120 mins"`)
 
-	var runtime Runtime
-	err := json.Unmarshal(jsonData, &runtime)
-	if err != nil {
-		t.Errorf("Failed to unmarshal JSON: %v", err)
+		var runtime Runtime
+		err := json.Unmarshal(jsonData, &runtime)
+		if err != nil {
+			t.Errorf("Failed to unmarshal JSON: %v", err)
+		}
+
+		expectedRuntime := Runtime(120)
+		if runtime != expectedRuntime {
+			t.Errorf("Unexpected unmarshaled Runtime value. Expected: %d, Got: %d", expectedRuntime, runtime)
+		}
+	})
+}
+
+func TestRuntime_UnmarshalJSONSadPaths(t *testing.T) {
+	tests := []struct {
+		Name     string
+		jsonData []byte
+	}{
+		{"Invalid Json not numberical",
+			[]byte(`"abc mins"`),
+		},
+		{
+			"Invalid Json doesn't end in mins",
+			[]byte(`"123 minsx"`),
+		},
 	}
-
-	expectedRuntime := Runtime(120)
-	if runtime != expectedRuntime {
-		t.Errorf("Unexpected unmarshaled Runtime value. Expected: %d, Got: %d", expectedRuntime, runtime)
+	for _, tt := range tests {
+		t.Run(tt.Name, func(t *testing.T) {
+			var runtime Runtime
+			err := json.Unmarshal(tt.jsonData, &runtime)
+			if err == nil {
+				t.Errorf("Expected error, but not none")
+			}
+		})
 	}
 }
