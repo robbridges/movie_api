cd5ddffe02202235c944fc51880e72d3131b363d
Rob Bridges <Rmb101187@gmail.com>
2023-07-05 11:53:39 -0700

    Fixed type in sort params, can now sort asc or desc

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -193,7 +193,7 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 
 	input.Filters.Sort = app.readString(qs, "sort", "id")
 
-	input.Filters.SortSafeList = []string{"id", "title", "year", "runtime", "-id", "-title", "-year", "runtime"}
+	input.Filters.SortSafeList = []string{"id", "title", "year", "runtime", "-id", "-title", "-year", "-runtime"}
 
 	if data.ValidateFilters(v, input.Filters); !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
